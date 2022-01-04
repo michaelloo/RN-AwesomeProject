2a5ec8f471eade5ba527cb6aa48cfc74703ea3c6
Michael Loo <>
2022-01-04 12:50:12 +1100

    Use StyleSheet to style previous context

--- PresentationalContext.js
+++ PresentationalContext.js
@@ -1,10 +1,25 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 const PresentationalContext = (props) => {
     return <View>
-        <Text onPress={props.updateState}>{props.myState}</Text>
+        <Text
+         onPress= { props.updateState }
+         style = { styles.myState }
+        >
+            {props.myState}
+        </Text>
     </View>
 }
 
 export default PresentationalContext
+
+const styles = StyleSheet.create ({
+    myState: {
+        marginTop: 20,
+        textAlign: 'center',
+        color: 'blue',
+        fontWeight: 'bold',
+        fontSize: 20
+    }
+})
